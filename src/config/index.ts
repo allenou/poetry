16ebfeee0223f637d3127f776e8b274aae7d3514
欧阳群孝 <ouyangqunxiao@microconnect.com>
2025-10-22 09:19:18 +0800

    fix: update paths in configuration to use Chinese titles

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 export const BASE_URL = 'https://cdn.statically.io/gh/chinese-poetry/chinese-poetry/master'
 
 export const PATHS = {
-  shijing: 'shijing/shijing.json',
-  lunyu: 'lunyu/lunyu.json',
-  caocao: 'caocaoshiji/caocao.json',
-  daxue: 'sishuwujing/daxue.json',
-  zhongyong: 'sishuwujing/zhongyong.json',
-  mengzi: 'sishuwujing/mengzi.json',
-  youmengying: 'youmengying/youmengying.json',
-  yuanqu: 'yuanqu/yuanqu.json'
+  shijing: '诗经/shijing.json',
+  lunyu: '论语/lunyu.json',
+  caocao: '曹操诗集/caocao.json',
+  daxue: '四书五经/daxue.json',
+  zhongyong: '四书五经/zhongyong.json',
+  mengzi: '四书五经/mengzi.json',
+  youmengying: '幽梦影/youmengying.json',
+  yuanqu: '元曲/yuanqu.json'
 }
